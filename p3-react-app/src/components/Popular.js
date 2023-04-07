8b5c89dd763a1d90b8f500ced3eb1a14e9c7fafc
reneetanoco <renee.tanoco@gmail.com>
2023-04-07 15:02:49 +0800

    feat(p3-react-app): added little styling and local storage

--- p3-react-app/src/components/Popular.js
+++ p3-react-app/src/components/Popular.js
@@ -12,10 +12,21 @@ function Popular()
   },[]);
   const getPopularRecipes  = async () =>
   {
-    const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=5`) // ctrl + c when implementing .env
-    const data = await api.json(); 
-    console.log(data);
-    setPopularRecipes(data.recipes)
+    const checkLocal = localStorage.getItem('popular'); // prevents data from fetching always if theres is local storage
+
+    if(checkLocal) // if there is local storage, no need to fetch datajust load what's inside local storage
+    {
+      setPopularRecipes(JSON.parse(checkLocal));
+    }
+    else
+    {
+      const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=4`) // ctrl + c when implementing .env, env. if a way to hide api key.
+      const data = await api.json(); 
+      console.log(data);
+      setPopularRecipes(data.recipes)
+      
+      localStorage.setItem('popular', JSON.stringify(data.recipes));
+    }
   };
   return (
     <div className='popular_recipes_container'>
@@ -24,8 +35,8 @@ function Popular()
           {
             return(
               <div className='popular_recipes'key ={recipes.id}>
-                <p className='recipes_title'>{recipes.title}</p>
                 <img src={recipes.image} alt={recipes.title}/>
+                <p className='recipes_title'>{recipes.title}</p>
               </div>
 
             );
