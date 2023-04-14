2b3161193edc752aee1b6a7cc7b93598f1f5310f
reneetanoco <renee.tanoco@gmail.com>
2023-04-14 19:48:15 +0800

    feat(p3-react-app): added comments on Recipe.js

--- p3-react-app/src/Page/Recipe.js
+++ p3-react-app/src/Page/Recipe.js
@@ -6,10 +6,10 @@ import Comment from '../components/Comment'
 
 function Recipe() 
 {
-  let params = useParams();
-  const [ingredients, setIngredients] = useState({});
+  let params = useParams(); // will extract name from url
+  const [ingredients, setIngredients] = useState({}); // 
 
-  const fetchRecipes = async() =>
+  const fetchRecipes = async() => // I wanted to try different ways to fetch data from api.
   {
     const data = await fetch(
       `https://api.spoonacular.com/recipes/${params.name}/information?apiKey=${process.env.REACT_APP_API_KEY}`
@@ -29,12 +29,11 @@ function Recipe()
 
   const handleBookmark = () => 
   {
-    const bookmarkedRecipes = JSON.parse(localStorage.getItem("bookmarkedRecipes")) || [];
-    const isAlreadyBookmarked = bookmarkedRecipes.find(recipe => recipe.id === ingredients.id);
-    
-    if (isAlreadyBookmarked) 
+    const bookmarkedRecipes = JSON.parse(localStorage.getItem("bookmarkedRecipes")) || []; // if nothing is stored in "bookmarkedRecipes", the fallback value || [] will assigns it to an empty array 
+     
+    if (bookmarkedRecipes.find(recipe => recipe.id === ingredients.id)) 
     {
-      const filteredRecipes = bookmarkedRecipes.filter(recipe => recipe.id !== ingredients.id);
+      const filteredRecipes = bookmarkedRecipes.filter(recipe => recipe.id !== ingredients.id); // filter out the 
       localStorage.setItem("bookmarkedRecipes", JSON.stringify(filteredRecipes));
     } 
     else 
